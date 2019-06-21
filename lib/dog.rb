078c18a43f74a2a2774316bcbcd26eead4e2e5ae
Valena Del Valle <valenadelvalle4@gmail.com>
2019-06-21 15:53:28 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog 
+
+  def name= (fido)
+    this_dogs_name = fido
+  end 
+
+  def name
+    @name 
+  end 
+
+  def name=(fido)
+    @name = fido 
+  end
+
+  def breed=("Beagle")
+    this_dogs_breed = "Beagle"
+  end 
+  
+  def breed 
+    @breed 
+  end 
+  
+  def breed=("Beagle")
+    @breed = "Beagle"
+  end
+  
+end
